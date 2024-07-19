6b390c1ef70508aa93715afaf09074e16af2b092
Zartab Nakhwa <zartab@codewithz.com>
2024-07-19 10:29:55 +0530

    Added Config Class

---
+++ src/main/java/com/inorg/config/ProjectSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.inorg.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+//        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
+//        http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());
+        http.authorizeHttpRequests((requests)->{
+            requests
+                    .requestMatchers("/accounts","/balance","/loans","/cards").authenticated()
+                    .requestMatchers("/notices","/contact","/error").permitAll();
+        });
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+}

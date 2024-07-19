55eb940dbacc7b8e0f957dcfb6af7a7ea0521546
Zartab Nakhwa <zartab@codewithz.com>
2024-07-19 09:53:04 +0530

    Created first controller and accessed using default user : 'user'

---
+++ src/main/java/com/inorg/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.inorg.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Welcome to InOrg Security July 2024";
+    }
+}

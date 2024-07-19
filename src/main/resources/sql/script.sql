0648a5ad82584913e52311a2b2f23168b81b7320
Zartab Nakhwa <zartab@codewithz.com>
2024-07-19 13:47:53 +0530

    AuthN & AuthZ : SQL Queries

--- src/main/resources/sql/script.sql
+++ src/main/resources/sql/script.sql
@@ -41,3 +41,54 @@ CREATE TABLE customer (
 INSERT INTO customer (email, pwd, role) VALUES
                                             ('happy@example.com', '{noop}inorg@12345', 'read'),
                                             ('admin@example.com', '{bcrypt}$2a$12$f0OhQ8.5zVYfXY87JcsQcOfwb7LezvhYx3OLGUzNlawBVrQnIXcWm', 'admin');
+
+--- Changes for AuthN and AuthZ
+
+-- ----------- Query changes for AuthN & AuthZ module ---------------
+
+-- Drop table if it exists
+DROP TABLE IF EXISTS customer;
+
+-- Create customer table
+CREATE TABLE customer (
+                          customer_id SERIAL PRIMARY KEY,
+                          name VARCHAR(100) NOT NULL,
+                          email VARCHAR(100) NOT NULL,
+                          mobile_number VARCHAR(20) NOT NULL,
+                          pwd VARCHAR(500) NOT NULL,
+                          role VARCHAR(100) NOT NULL,
+                          create_dt DATE DEFAULT NULL
+);
+-- $2a$12$pHuvQ9nb/R6axX.x2ZpuJuoWAE1DzvQNzT21Ovnzvdj4OH4ViGe6. -- cwz@123
+-- Insert data into customer table
+INSERT INTO customer (name, email, mobile_number, pwd, role, create_dt)
+VALUES
+    ('Tom', 'tom@example.com', '9876548337', '{bcrypt}$2a$12$pHuvQ9nb/R6axX.x2ZpuJuoWAE1DzvQNzT21Ovnzvdj4OH4ViGe6.', 'admin', CURRENT_DATE),
+    ('Alex', 'alex@example.com', '9876548337', '{bcrypt}$2a$12$pHuvQ9nb/R6axX.x2ZpuJuoWAE1DzvQNzT21Ovnzvdj4OH4ViGe6.', 'admin', CURRENT_DATE),
+    ('Mike', 'mike@example.com', '9876548337', '{bcrypt}$2a$12$pHuvQ9nb/R6axX.x2ZpuJuoWAE1DzvQNzT21Ovnzvdj4OH4ViGe6.', 'admin', CURRENT_DATE);
+
+-- Drop table if it exists
+DROP TABLE IF EXISTS authorities;
+
+-- Create authorities table
+CREATE TABLE authorities (
+                             id SERIAL PRIMARY KEY,
+                             customer_id INT NOT NULL,
+                             name VARCHAR(50) NOT NULL,
+                             FOREIGN KEY (customer_id) REFERENCES customer (customer_id)
+);
+
+-- Insert data into authorities table
+INSERT INTO authorities (customer_id, name)
+VALUES
+    (1, 'READ'),
+    (1, 'WRITE');
+
+INSERT INTO authorities (customer_id, name)
+VALUES
+    (2, 'READ');
+
+
+INSERT INTO authorities (customer_id, name)
+VALUES
+    (3, 'WRITE');

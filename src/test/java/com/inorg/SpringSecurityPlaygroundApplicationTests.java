d73bac7fda925a9fd032e0f906b1e049e6709f35
Zartab Nakhwa <zartab@codewithz.com>
2024-07-19 09:38:54 +0530

    Initial Commit

---
+++ src/test/java/com/inorg/SpringSecurityPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inorg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

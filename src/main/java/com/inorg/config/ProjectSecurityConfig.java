81438ca1c61f347dbf2102d69f6902e79d912a43
Zartab Nakhwa <zartab@codewithz.com>
2024-07-19 10:53:04 +0530

    Added Bean for UserDetails using InMemoryUserDetailsManager

--- src/main/java/com/inorg/config/ProjectSecurityConfig.java
+++ src/main/java/com/inorg/config/ProjectSecurityConfig.java
@@ -3,6 +3,10 @@ package com.inorg.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -23,5 +27,17 @@ public class ProjectSecurityConfig {
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        UserDetails user= User.withUsername("user").
+                            password("password").authorities("READ").build();
+
+        UserDetails admin= User.withUsername("admin").
+                password("54321").authorities("ADMIN").build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
